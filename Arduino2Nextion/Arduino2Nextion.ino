37d703c641bbbf1a92dcacf785d16a3165e031b4
“Min04C” <emelynma099@gmail.com>
2024-04-20 16:24:49 -0400

    Clean Up

--- Arduino2Nextion/Arduino2Nextion.ino
+++ Arduino2Nextion/Arduino2Nextion.ino
@@ -1,4 +1,4 @@
-#include <NexButton.h>
+/*#include <NexButton.h>
 #include <NexCheckbox.h>
 #include <NexConfig.h>
 #include <NexCrop.h>
@@ -23,111 +23,77 @@
 #include <NexVariable.h>
 #include <NexWaveform.h>
 #include <Nextion.h>
-#include <doxygen.h>
- 
-int looper = 0;
-bool fan = false;
+#include <doxygen.h>*/
+
+//----------------------------------------------------------------------
+// When Done, remove lines (and any relating code) that have a comment:
+//                          "for testing"
+//----------------------------------------------------------------------
+
+//int looper = 0;
+//bool fan = false;
+int i; // for testing
 
 void setup() {
   // put your setup code here, to run once:
+  i = 50; // for testing
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   // Coolant Temp
-  /*int CoolVal = 22;
-  Serial.print("coolant.val=" + String(CoolVal));
-  Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+  int CoolVal = 39;
+  Serial.print("coolant.val=" + String(CoolVal)); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
 
   // Fan
-  String FanVal = "OFF";
-  Serial.print("fan.txt=" + String(FanVal));
-  Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+  String FanVal = "\"ON\""; // Options: "\"ON\"", "\"OFF\"" - maybe "\"AUTO\""
+  Serial.print("fan.txt=" + FanVal); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
 
   // Battery
-  //for(int i = 0; i < 10; i++) {
-    Serial.print("battery.val=" + String(10));
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-  //}
-  //int BatteryVal = 2;
-  //Serial.print("battery.val=" + String(BatteryVal));
-  //Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+  int BatteryVal = 2;
+  Serial.print("battery.val=" + String(BatteryVal)); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
 
-  // Gear - Needs fixing for both int (1-6) and string ("N")
-  String GearVal = "1";
-  Serial.print("gear.txt=" + GearVal);
-  Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+  // Gear 
+  String GearVal = "\"1\""; // Options: "\"N\"" or "\"i\"" where i = 0,1,2,3,4,5,6 - maybe not 0
+  Serial.print("gear.txt=" + GearVal); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
 
   // Traction Control
-  int TCVal = 1; // 0 = OFF, 1 = ON
-  Serial.print("tc.val=" + String(TCVal));
-  Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+  int TCVal = 1; // Options: 0 = OFF, 1 = ON
+  Serial.print("tc.val=" + String(TCVal)); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
 
   // Launch Control
-  int LCVal = 1; // 0 = OFF, 1 = ON
-  Serial.print("lc.val=" + String(LCVal));
-  Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-*/
-  // RPM1 = 2x Progress Bars
+  int LCVal = 1; // Options: 0 = OFF, 1 = ON
+  Serial.print("lc.val=" + String(LCVal)); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+
+  // RPM 
   // Have rmp brought down from 13,500 --> 135 (so divided by 100)
-  for(int i = 50; i < 120; i++) {
-    int RPMVal = i;
-    /*if (RPMVal <= 100) {
-    Serial.print("p1.val=" + String(RPMVal));
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-    Serial.print("p2.val=0");
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-  } else if (RPMVal > 100) {
-    int RPMP2 = (RPMVal % 100);
-    int RPMP1 = (RPMVal - RPMP2);
-    RPMP2 = (RPMP2 * 100) / 35;
-    Serial.print("p1.val=" + String(RPMP1)); 
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-    Serial.print("p2.val=" + String(RPMP2));
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-  }*/
+  if (i > 135) { // for testing
+    i = 50; // for testing
+  } // for testing
+  int RPMVal = i++; 
   
-  /*if (RPMVal <= 100) {
-    Serial.print("p1.val=" + String(RPMVal));
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-    Serial.print("p2.val=0");
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+  if (RPMVal <= 100) {
+    Serial.print("p1.val=" + String(RPMVal)); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+    Serial.print("p2.val=0"); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
   } else if (RPMVal > 100) {
     int RPMP2 = (RPMVal % 100);
     int RPMP1 = (RPMVal - RPMP2);
     RPMP2 = (RPMP2 * 100) / 35;
-    Serial.print("p1.val=" + String(RPMP1)); 
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-    Serial.print("p2.val=" + String(RPMP2));
-    Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-  }*/
-
-  // RPM2 = 27x Progress Bars
-  //int RPMVal = 101; // // Have rmp brought down from 13,500 --> 135 (so divided by 100)
-  String rpmBars[27] = {"j0", "j1", "j2", "j3", "j4", "j5", "j6", "j7", "j8", "j9", "j10", "j11", "j12", "j13", "j14", "j15", "j16", "j17", "j18", "j19", "j20", "j21", "j22", "j23", "j24", "j25", "j26"};
-  int RPMBarVal;
-    //int RPMVal = i;
-    for (size_t i = 0; i < 27; ++i) {
-      int low = (i*5);
-      int high = (low + 5);
-      if (RPMVal > low) {
-        RPMBarVal = 100;
-      } else {
-        RPMBarVal = 0;
-      }
-      Serial.print(rpmBars[i] + ".val=" + String(RPMBarVal));
-      Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
-    }
+    Serial.print("p1.val=" + String(RPMP1)); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+    Serial.print("p2.val=" + String(RPMP2)); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+    // ONCE EVERYTHING COMPLETELY WORKS - CHANGE TOP CODE TO BELOW CODE (FASTER):
+      // NOTE: This will not work if the Arduino ends up recieving out-of-bound rpm values from MOTEC/CAN
+    //int RPMP2 = (RPMVal % 100) * 100 / 35;
+    //Serial.print("p1.val=100"); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
+    //Serial.print("p2.val=" + String(RPMP2)); Serial.write(0xff); Serial.write(0xff); Serial.write(0xff);
   }
-  // Ranges:
-  // {0, 1, 2, 3, 4, 5...}
-  // {0-5, 6-10, 11-15...}*/
-
+  
   //delay(10);
 }
 
-void changeVal() {
+/*void changeVal() {
   for(int i = 0; i < 13500; i++) {
     looper = i;
     Serial.println(looper);
@@ -142,4 +108,4 @@ void changeVal() {
     Serial.println("fan off");
   }
   fan = !fan;
-}
+}*/
